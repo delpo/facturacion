cef17f4e87b81e7a8c5ad5ddd59885f9d26fbd05
Ángel Carlos del Pozo Muela <carlosdc360@gmail.com>
2013-05-10 14:00:16 +0200

    v50
    Añadir tarifa casi hecho

--- src/gui/EscuchadorBotonModificarTarifa.java
+++ src/gui/EscuchadorBotonModificarTarifa.java
@@ -1,11 +1,21 @@
 package gui;
 
+import java.awt.BorderLayout;
+import java.awt.Container;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 
+import facturacion.ExcepcionNIFnoValido;
+import facturacion.NIF;
 import facturacion.Operador;
 
 public class EscuchadorBotonModificarTarifa implements ActionListener {
@@ -21,8 +31,56 @@ public class EscuchadorBotonModificarTarifa implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent arg0) {
-		// TODO Auto-generated method stub
+		ventana.dispose();
+		boolean todo_ok = true;
+		NIF nif_valido = null;
+		try {
+			nif_valido = new NIF(nif.getText());
+		} catch (ExcepcionNIFnoValido e) {
+			JOptionPane.showMessageDialog(null, "NIF NO VÁLIDO.");
+			todo_ok = false;
+		}
+		if(todo_ok){
+			JFrame ventana = new JFrame("Añadir tarifa");
+			Container contenedor = ventana.getContentPane();
+			contenedor.setLayout(new BoxLayout(contenedor, BoxLayout.PAGE_AXIS));
+			String html = "<html>" +
+	                "<b>Paso 2: </b><br/>" +
+	                " <i>Seleccionar tarifa.</i><br/>--------------------------" +
+	                "</html>";
+	    	JLabel etiqueta = new JLabel(html);
+	    	ventana.getContentPane().add(etiqueta, BorderLayout.NORTH);
+	    	ventana.setAlwaysOnTop(true);
+			
+	    	//do stuff
+	    	JCheckBox tarde = new JCheckBox("Tarifa de tarde");
+	    	JCheckBox domingo = new JCheckBox("Tarifa de domingo");
+	    	JPanel tarifas = new JPanel();
+	    	tarifas.setLayout(new BoxLayout(tarifas, BoxLayout.PAGE_AXIS));
+	    	contenedor.add(tarifas);//Cambiamos el panel contenedor
+	    	tarifas.add(new JLabel("Elije tarifas a añadir:"));
+	    	tarifas.add(tarde);
+	    	tarifas.add(domingo);
+	    	ventana.pack();
+	    	ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    	ventana.setVisible(true);
+	    	
+	    	//Botón de aceptar
+	    	JButton ok = new JButton("Aceptar");
 
+	    	//ok.addActionListener(new EscuchadorBotonEmpresaOK(ventana, op, nombre, nif, cp, poblacion, provincia, email));
+
+	    	ventana.getContentPane().add(ok);
+	    	
+	    	
+			ventana.setSize(370, 500);
+		    ventana.setResizable(false);
+		    ventana.setLocationRelativeTo(null);
+			ventana.pack();
+			ventana.setVisible(true);
+		}else{
+			JOptionPane.showMessageDialog(null, "ERROR: DNI NO VÁLIDO.");
+		}
 	}
 
 }
